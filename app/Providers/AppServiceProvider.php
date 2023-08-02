e05ab767c6be87db16a7cb1055ee825616f69e03
Neno Arisma <nenoarisma@gmail.com>
2023-08-02 19:31:08 +0700

    chore: import left appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Laravel\Passport\Console\InstallCommand;
+use Laravel\Passport\Console\ClientCommand;
+use Laravel\Passport\Console\KeysCommand;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider

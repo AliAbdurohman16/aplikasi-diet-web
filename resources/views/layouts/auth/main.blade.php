fb1b2f873e7a74c103fa031631001bfb5f060be5
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-09-20 12:15:38 +0700

    change title

--- resources/views/layouts/auth/main.blade.php
+++ resources/views/layouts/auth/main.blade.php
@@ -3,7 +3,7 @@
 
     <head>
         <meta charset="utf-8" />
-        <title>@yield('title') - Aplikasi Diet</title>
+        <title>@yield('title') - HEPAN (Health Planner)</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <!-- favicon -->
--- resources/views/layouts/backend/main.blade.php
+++ resources/views/layouts/backend/main.blade.php
@@ -3,7 +3,7 @@
 
     <head>
         <meta charset="utf-8" />
-        <title>@yield('title') - Aplikasi Diet</title>
+        <title>@yield('title') - HEPAN (Health Planner)</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 

bc6ce4cdc90ecf78a0e20fc53937cf6171391425
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-11-08 09:37:59 +0700

    remove abdominal_circumference

--- database/migrations/2023_07_03_143801_create_anthropometry_table.php
+++ database/migrations/2023_07_03_143801_create_anthropometry_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id');
             $table->integer('weight');
             $table->integer('height');
-            $table->integer('abdominal_circumference');
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });

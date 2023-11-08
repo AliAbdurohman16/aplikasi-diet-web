d7402b93c8e8699df5acfe7ac45ed08e39b5b913
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-11-08 12:58:02 +0700

    change migrations

--- database/migrations/2023_07_03_142517_create_histories_table.php
+++ database/migrations/2023_07_03_142517_create_histories_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('reports', function (Blueprint $table) {
+        Schema::create('histories', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->string('calories');

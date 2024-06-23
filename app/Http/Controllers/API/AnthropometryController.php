4cf2fbee78e4e4387b9d2530a21829576170996e
Neno Arisma <nenoarisma@gmail.com>
2024-06-23 13:17:58 +0700

    chore: update Anthropometry controller
    add model to store the data for showing in history

--- app/Http/Controllers/API/AnthropometryController.php
+++ app/Http/Controllers/API/AnthropometryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Anthropometry;
 use Illuminate\Http\Request;
 use App\Helpers\ResponseFormatter;
 
@@ -23,15 +24,15 @@ class AnthropometryController extends Controller
 
         if ($imt < 18.5) {
             $result = 'Underweight';
-        } else if ($imt >= 18.5 && $imt <= 24.9) {
+        } elseif ($imt >= 18.5 && $imt <= 24.9) {
             $result = 'Normal';
-        } else if ($imt >= 25 && $imt <= 29.9) {
+        } elseif ($imt >= 25 && $imt <= 29.9) {
             $result = 'Overweight';
-        } else if ($imt >= 30 && $imt <= 34.9) {
+        } elseif ($imt >= 30 && $imt <= 34.9) {
             $result = 'Obesity Class I';
-        } else if ($imt >= 35 && $imt <= 39.9) {
+        } elseif ($imt >= 35 && $imt <= 39.9) {
             $result = 'Obesity Class II';
-        } else if ($imt >= 40) {
+        } elseif ($imt >= 40) {
             $result = 'Obesity Class III';
         }
 
@@ -40,6 +41,21 @@ class AnthropometryController extends Controller
             'result' => $result,
         ];
 
+
+
+        Anthropometry::create([
+            'result' => $result,
+            'imt' => $imt
+        ]);
+
         return ResponseFormatter::success($data, 'Kalkulasi IMT berhasil!');
+    }
+
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+
     }
 }

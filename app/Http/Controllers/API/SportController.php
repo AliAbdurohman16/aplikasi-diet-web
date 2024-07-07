7d2332ef5e7bbf5afa8591e7eb48106620d05f32
Neno Arisma <nenoarisma@gmail.com>
2024-07-07 20:59:07 +0700

    chore: Add user_id filter to history queries in SportController

--- app/Http/Controllers/API/SportController.php
+++ app/Http/Controllers/API/SportController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Sport;
 use App\Helpers\ResponseFormatter;
+use App\Models\History;
+use Illuminate\Support\Facades\Auth;
 
 class SportController extends Controller
 {
@@ -14,5 +16,23 @@ class SportController extends Controller
         $sports = Sport::all();
 
         return ResponseFormatter::success(['sports' => $sports], 'Data berhasil ditampilkan!');
+    }
+
+
+    public function store(Request $request)
+    {
+        $sport = History::create([
+            'user_id' => Auth::user()->id,
+            'name' => $request->name,
+            'duration' => $request->duration,
+            'calories' => $request->calories,
+            'category' => $request->category,
+            'protein' => '0',
+            'fat' => '0',
+            'carbohydrates' => '0',
+
+        ]);
+
+        return ResponseFormatter::success($sport, 'Data berhasil ditambahkan!');
     }
 }
